d02fbf115e4611a08a5647fb347167d7e231dd95
Dinama-20 <abrahamdc2004@gmail.com>
2025-04-14 22:44:03 +0300

    Updating

--- public/generate_pdf.php
+++ public/generate_pdf.php
@@ -38,6 +38,10 @@ $pdf->Cell(0, 10, "Date and Time: " . date('Y-m-d H:i:s'), 0, 1);
 // Clear the cart after generating the PDF
 unset($_SESSION['cart']);
 
+// Reload the page
+header('Location: cart.php');
+exit;
+
 // Send the PDF to the browser for download
 $pdf->Output('D', 'order-details.pdf');
 exit;
